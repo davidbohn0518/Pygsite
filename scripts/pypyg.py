b1bc4267663b9c007e1b1c8437bb5976aa29e65d
David Bohn <bohn.david.j@gmail.com>
2022-06-24 23:00:00 -0500

    added handling for upper characters

--- scripts/pypyg.py
+++ scripts/pypyg.py
@@ -6,7 +6,7 @@ CSUFFIX = "ay"
 CLUSTER = ['pl', 'pr', 'bl', 'br', 'tr', 'dr', 'kl', 'kr', 'gl',
            'gr', 'fl', 'fr', 'shr', 'sk', 'skr', 'sl', 'sm',
            'sn', 'sp', 'spl', 'spr', 'st', 'str', 'sw', 'tw',
-           'dw', 'kw', 'skw', 'gw']
+           'dw', 'kw', 'skw', 'gw', 'qu', 'sch']
 
 
 def pygify(string: str):
@@ -14,24 +14,36 @@ def pygify(string: str):
     for word in string.split(" "):
         word.strip(SPECIAL_CHARS)
         fLetter = word[0]
-        if word[:3].casefold() in CLUSTER:
-            word = word[3:] + word[:3] + CSUFFIX
-        elif word[:2].casefold() in CLUSTER:
-            word = word[2:] + word[0:2] + CSUFFIX
-        elif fLetter.casefold() in VOWELS:
-            word = word[1:] + fLetter + VSUFFIX
+        f2Letters = word[:2]
+        f3letters = word[:3]
+        if fLetter.isupper():
+            if f3letters.casefold() in CLUSTER:
+                word = word[3:] + f3letters.lower() + CSUFFIX
+            elif f2Letters.casefold() in CLUSTER:
+                word = word[2:] + f2Letters.lower() + CSUFFIX
+            elif fLetter.casefold() in VOWELS:
+                word = word[1:] + fLetter.lower() + VSUFFIX
+            else:
+                word = word[1:] + fLetter.lower() + CSUFFIX
+            word = word.capitalize()
+
         else:
-            word = word[1:] + fLetter + CSUFFIX
+            if word[:3].casefold() in CLUSTER:
+                word = word[3:] + word[:3] + CSUFFIX
+            elif word[:2].casefold() in CLUSTER:
+                word = word[2:] + word[0:2] + CSUFFIX
+            elif fLetter.casefold() in VOWELS:
+                word = word[1:] + fLetter + VSUFFIX
+            else:
+                word = word[1:] + fLetter + CSUFFIX
+
         rstring += word + " "
     print(rstring)
+    return rstring
 
 
-# def handleConsonent(word: str, fLetter):
-#     word[1:] + fLetter + CSUFFIX
-#     return word
+def depygify():
+    pass
 
 
-pygify("small")
-pygify("Small")
-pygify('shrink')
-pygify('Shrink')
+pygify("Small big Olive spring Schedule")
