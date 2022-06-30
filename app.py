4b0c712ef0638ae23c839d960d759b59041d1a22
David Bohn <bohn.david.j@gmail.com>
2022-06-30 07:05:59 -0500

    converted api to restful api

--- app.py
+++ app.py
@@ -1,24 +1,30 @@
 from flask import Flask, jsonify, request
+from flask_restful import Api, Resource
 from pyscripts import pypyg
 
 
 app = Flask(__name__)
+api = Api(app)
 
 
-@app.route("/convert-to", methods=['POST'])
-def convert_to():
-    data = request.get_json()
-    retStr = pypyg.pygify(data["str"])
-    retJSON = {"string": retStr}
-    return jsonify(retJSON)
+class Pygify(Resource):
+    def post(self):
+        data = request.get_json()
+        retStr = pypyg.pygify(data["str"])
+        retJSON = {"string": retStr}
+        return jsonify(retJSON)
 
 
-@app.route("/convert-from", methods=['POST'])
-def convert_from():
-    data = request.get_json()
-    retStr = pypyg.depygify(data["str"])
-    retJSON = {"string": retStr}
-    return jsonify(retJSON)
+class Depygify(Resource):
+    def post(self):
+        data = request.get_json()
+        retStr = pypyg.depygify(data["str"])
+        retJSON = {"string": retStr}
+        return jsonify(retJSON)
+
+
+api.add_resource(Pygify, "/convert-to")
+api.add_resource(Depygify, "/convert-from")
 
 
 if __name__ == '__main__':
