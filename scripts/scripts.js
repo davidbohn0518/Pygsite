c4d61be1dc0f4654dc0889f61a027a74da67ec4b
David Bohn <bohn.david.j@gmail.com>
2022-07-04 16:09:11 -0500

    added fetch promise handling for data transfer to app.py api

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,25 +1,49 @@
-const api = '127.0.0.1:5000'
+const api = 'http://127.0.0.1:5000';
+const formEng = document.querySelector('#formEng');
+const formPyg = document.querySelector('#formPyg');
 
-// let btn1 = document.querySelector()
-
-document.querySelector('#formEng').addEventListener('submit', function(e){
+formEng.addEventListener('submit', function(e){
     e.preventDefault();
-    let data = e.target.elements.inputEng.value
-    console.log(data);
-    console.log(JSON.stringify(data));
-    // let options = {
-    //     method: 'GET',
-    //     headers: {
-    //         'Content-Type': 'application/json'
-    //     },
-    //     body: JSON.stringify(data),
-    // };
-
-    // fetch(api, options);
+    let data = {str: e.target.elements.inputEng.value};
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data),
+    };
+    fetch(api + "/convert-to", options)
+    .then(function (response) {
+        if (response.ok) {
+            return response.json();
+        }
+        return Promise.reject(response);
+    }).then(function (data) {
+        document.querySelector("#inputPyg").value = data["str"]
+    }).catch(function (error) {
+        console.warn('Something went wrong.', error)
+    })
 })
 
-document.querySelector('#formPyg').addEventListener('submit', function(e){
+formPyg.addEventListener('submit', function(e){
     e.preventDefault();
-    let data = e.target.elements.inputPyg.value
-    console.log(data)
+    let data = {str: e.target.elements.inputPyg.value};
+    let options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data),
+    };
+    fetch(api + "/convert-from", options)
+    .then(function (response) {
+        if (response.ok) {
+            return response.json();
+        }
+        return Promise.reject(response);
+    }).then(function (data) {
+        document.querySelector("#inputEng").value = data["str"]
+    }).catch(function (error) {
+        console.warn('Something went wrong.', error)
+    })
 })
