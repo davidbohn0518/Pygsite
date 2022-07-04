743e228eb8e59025270f6c85cea53308e61b2d2c
David Bohn <bohn.david.j@gmail.com>
2022-07-04 16:07:59 -0500

    updated api with CORS

--- app.py
+++ app.py
@@ -1,17 +1,21 @@
 from flask import Flask, jsonify, request
 from flask_restful import Api, Resource
+from flask_cors import CORS
 from pyscripts import pypyg
 
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 
 
 class Pygify(Resource):
     def post(self):
         data = request.get_json()
-        retStr = pypyg.pygify(data["str"])
-        retJSON = {"string": retStr}
+        print(data)
+        retStr = pypyg.pygify(data['str'])
+        retJSON = {'str': retStr}
+        print(retJSON)
         return jsonify(retJSON)
 
 
@@ -19,7 +23,7 @@ class Depygify(Resource):
     def post(self):
         data = request.get_json()
         retStr = pypyg.depygify(data["str"])
-        retJSON = {"string": retStr}
+        retJSON = {"str": retStr}
         return jsonify(retJSON)
 
 
